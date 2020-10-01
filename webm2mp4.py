6348bdb49e50d47ba74e1e0dae312aee1808b705
n0nuser <pjgr2000@gmail.com>
2020-10-01 14:20:45 +0200

    Fix outputfile

--- webm2mp4.py
+++ webm2mp4.py
@@ -3,28 +3,29 @@ import argparse
 import os
 import subprocess
 
-# ffmpeg needed
-
 def args():
     desc = "Convert webm to mp4."
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("-i", help="Name of Input File", type=str, dest="input")
     parser.add_argument("-o", help="Name of Output File", type=str, dest="output")
-    inp = parser.parse_args().input
-    out = parser.parse_args().output
+    inp = str(parser.parse_args().input)
+    out = str(parser.parse_args().output)
     if inp is None or out is None:
-        print("usage: webm2mp4.py [-h] [-i INPUT] [-o OUTPUT]!")
+        print("usage: webm2mp4.py [-h] [-i INPUT] [-o OUTPUT]")
+        exit()
+    if not out.endswith(".mp4",4):
+        print("Output file is not .mp4!!")
         exit()
     return inp,out
 
 
 inp, out = args()
 
+# Verifies ffmpeg is installed
 status, result = subprocess.getstatusoutput("which ffmpeg")
 if status == 1:
     print("ffmpeg needed!\nTo install it: sudo apt install ffmpeg -y")
 
-mp4_file = inp.replace('.webm','.mp4')
-cmd_string = 'ffmpeg -i "' + inp + '" "' + mp4_file + '"'
-print('converting ' + inp + ' to ' + mp4_file)
+cmd_string = 'ffmpeg -i "' + inp + '" "' + out + '"'
+print('converting ' + inp + ' to ' + out)
 os.system(cmd_string)
