0f9106e41cfb869d42f5a8fda0e67149cbcc0d05
Pablo González Rubio <pjgr2000@gmail.com>
2020-12-09 13:35:20 +0100

    Add files via upload

--- lichessAPI.py
+++ lichessAPI.py
@@ -4,43 +4,46 @@
 # it retrieves all the player statistics
 
 import lichess.api
+def retrieve(object,game,data):
+    return str(object['perfs'][game][data])
 
 user = str(input("Introduce your user: "))
 try:
     userObject = lichess.api.user(user)
     
     print("\nBULLET\n------")
-    print("- Games: " + str(userObject['perfs']['bullet']['games']))
-    print("- Rating: " + str(userObject['perfs']['bullet']['rating']))
-    print("- Rating Deviation: " + str(userObject['perfs']['bullet']['rd']))
-    print("- Progress: " + str(userObject['perfs']['bullet']['prog']))
+    print("- Games: " + retrieve(userObject,'bullet','games'))
+    print("- Rating: " + retrieve(userObject,'bullet','rating'))
+    print("- Rating Deviation: " + retrieve(userObject,'bullet','rd'))
+    print("- Progress: " + retrieve(userObject,'bullet','prog'))
     
     print("\nBLITZ\n-----")
-    print("- Games: " + str(userObject['perfs']['blitz']['games']))
-    print("- Rating: " + str(userObject['perfs']['blitz']['rating']))
-    print("- Rating Deviation: " + str(userObject['perfs']['blitz']['rd']))
-    print("- Progress: " + str(userObject['perfs']['blitz']['prog']))
+    print("- Games: " + retrieve(userObject,'blitz','games'))
+    print("- Rating: " + retrieve(userObject,'blitz','rating'))
+    print("- Rating Deviation: " + retrieve(userObject,'blitz','rd'))
+    print("- Progress: " + retrieve(userObject,'blitz','prog'))
 
     print("\nRAPID\n-----")
-    print("- Games: " + str(userObject['perfs']['rapid']['games']))
-    print("- Rating: " + str(userObject['perfs']['rapid']['rating']))
-    print("- Rating Deviation: " + str(userObject['perfs']['rapid']['rd']))
-    print("- Progress: " + str(userObject['perfs']['rapid']['prog']))
+    print("- Games: " + retrieve(userObject,'rapid','games'))
+    print("- Rating: " + retrieve(userObject,'rapid','rating'))
+    print("- Rating Deviation: " + retrieve(userObject,'rapid','rd'))
+    print("- Progress: " + retrieve(userObject,'rapid','prog'))
 
     print("\nCLASSICAL\n---------")
-    print("- Games: " + str(userObject['perfs']['classical']['games']))
-    print("- Rating: " + str(userObject['perfs']['classical']['rating']))
-    print("- Rating Deviation: " + str(userObject['perfs']['classical']['rd']))
-    print("- Progress: " + str(userObject['perfs']['classical']['prog']))
+    print("- Games: " + retrieve(userObject,'classical','games'))
+    print("- Rating: " + retrieve(userObject,'classical','rating'))
+    print("- Rating Deviation: " + retrieve(userObject,'classical','rd'))
+    print("- Progress: " + retrieve(userObject,'classical','prog'))
 
     print("\nPUZZLE\n------")
-    print("- Games: " + str(userObject['perfs']['puzzle']['games']))
-    print("- Rating: " + str(userObject['perfs']['puzzle']['rating']))
-    print("- Rating Deviation: " + str(userObject['perfs']['puzzle']['rd']))
-    print("- Progress: " + str(userObject['perfs']['puzzle']['prog']))
+    print("- Games: " + retrieve(userObject,'puzzle','games'))
+    print("- Rating: " + retrieve(userObject,'puzzle','rating'))
+    print("- Rating Deviation: " + retrieve(userObject,'puzzle','rd'))
+    print("- Progress: " + retrieve(userObject,'puzzle','prog'))
     pass
 except KeyboardInterrupt:
     pass
 except:
     print("That user does not exist.")
+
 
